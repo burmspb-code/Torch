1f8fb38125a2e85e4ecb60cc29128a1d8bcd8ebb
Mikhail Burdukovsky <burmspb@gmail.com>
2026-08-03 09:52:05 +0300

    add simplejwt

--- config/settings.py
+++ config/settings.py
@@ -26,8 +26,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     "rest_framework",
     "django_filters",
+    "rest_framework_simplejwt",
+
     "users",
     "aurora",
 ]
@@ -116,5 +119,13 @@ MEDIA_ROOT = BASE_DIR / "media"
 AUTH_USER_MODEL = "users.CustomUser"
 
 REST_FRAMEWORK = {
-    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"]
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
