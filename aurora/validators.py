ba0bb24c2d8903be89d970a706f5a0cad6323f45
Mikhail Burdukovsky <burmspb@gmail.com>
2026-08-16 22:26:53 +0300

    correction of homework

--- aurora/validators.py
+++ aurora/validators.py
@@ -1,8 +1,9 @@
 """Валидаторы приложения aurora."""
 
+from urllib.parse import urlparse
 from rest_framework.serializers import ValidationError
 
-ALLOWED_WORDS = "youtube.com"
+ALLOWED_DOMAIN = {"youtube.com", "www.youtube.com", "youtu.be"}
 
 
 def validator_allowed_words(value):
@@ -11,5 +12,16 @@ def validator_allowed_words(value):
     if not value:
         return
 
-    if value not in ALLOWED_WORDS.lower():
-        raise ValidationError("Ссылка может быть только на ресурс youtube.com")
+    try:
+        parse_url = urlparse(str(value).strip()) # Парсим ссылку
+
+        host_name = parse_url.hostname # Получаем домен
+
+        if not host_name:
+            raise ValidationError("Передана не корректная ссылка")
+
+        if host_name.lower() not in ALLOWED_DOMAIN:
+            raise ValidationError("Ссылка может быть только на ресурс youtube.com")
+
+    except ValueError:
+        raise ValidationError("Не удалось распознать формат ссылки")
